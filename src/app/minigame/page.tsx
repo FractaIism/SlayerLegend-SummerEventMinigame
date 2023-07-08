1d660824f74beb85e4686dde411a1abff1969ab2
Fractalism <fractalism.cs05@nctu.edu.tw>
2023-07-08 22:55:24 +0800

    move minigame to subdirectory

--- src/app/minigame/page.tsx
+++ src/app/minigame/page.tsx
@@ -4,7 +4,12 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <Image
+        src="/images/minigame.png"
+        alt="minigame.png"
+        width={250}
+        height={250}
+      />
     </main>
   )
 }
