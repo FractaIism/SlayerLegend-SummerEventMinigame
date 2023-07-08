e9efcf8db02b8ee07b6147130e765ebdb4931d50
Fractalism <fractalism.cs05@nctu.edu.tw>
2023-07-09 07:12:46 +0800

    add input and styling

--- src/app/minigame/weights.tsx
+++ src/app/minigame/weights.tsx
@@ -4,6 +4,13 @@ export function Weights() {
   return (
     <div className={styles.weights}>
       <div className={`${styles.block} ${styles.diamond_100_5000}`} />
+      <input
+        className={styles.weightInput}
+        name={styles.weightInput}
+        type="text"
+        value={123}
+        placeholder="0"
+      />
     </div>
   );
 }
