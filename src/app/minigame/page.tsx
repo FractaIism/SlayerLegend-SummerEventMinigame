31c7fcb84cf99eeac3034d95effe35f8f55538c0
Fractalism <fractalism.cs05@nctu.edu.tw>
2023-07-09 00:32:43 +0800

    add slayer to page

--- src/app/minigame/page.tsx
+++ src/app/minigame/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const slayerBaseWidth = 84;
+  const slayerBaseHeight = 122;
+  const slayerScaleFactor = 0.4;
+
   return (
     <main className={styles.main}>
       <Image
@@ -10,6 +14,13 @@ export default function Home() {
         width={250}
         height={250}
       />
+      <Image
+        className={styles.slayer}
+        src="/images/slayer_summer.png"
+        width={slayerBaseWidth * slayerScaleFactor}
+        height={slayerBaseHeight * slayerScaleFactor}
+        alt="slayer_summer.png"
+      />
     </main>
-  )
+  );
 }
