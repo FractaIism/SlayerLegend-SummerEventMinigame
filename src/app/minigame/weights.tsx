8589efcd502e7ce935e76ee138d88f1c4d0d991d
Fractalism <fractalism.cs05@nctu.edu.tw>
2023-07-09 06:56:49 +0800

    move weights to the right of gameboard

--- src/app/minigame/weights.tsx
+++ src/app/minigame/weights.tsx
@@ -1,5 +1,9 @@
 import styles from "./page.module.scss";
 
 export function Weights() {
-  return <div className={`${styles.block} ${styles.diamond_100_5000}`} />;
+  return (
+    <div className={styles.weights}>
+      <div className={`${styles.block} ${styles.diamond_100_5000}`} />
+    </div>
+  );
 }
