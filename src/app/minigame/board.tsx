67ff56ca91743a2f85da3540599e0be0f46b7f88
Fractalism <fractalism.cs05@nctu.edu.tw>
2023-07-09 01:41:01 +0800

    extract GameBoard component

---
+++ src/app/minigame/board.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function GameBoard() {
+  return (
+    <Image
+      src="/images/minigame.png"
+      alt="minigame.png"
+      width={250}
+      height={250}
+    />
+  );
+}
--- src/app/minigame/page.tsx
+++ src/app/minigame/page.tsx
@@ -1,17 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Slayer from "./slayer.tsx";
+import GameBoard from "./board.tsx";
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
-      <Image
-        src="/images/minigame.png"
-        alt="minigame.png"
-        width={250}
-        height={250}
-      />
+      <GameBoard />
       <Slayer />
     </main>
   );
