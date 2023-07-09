72c6c58efae663810aebe26646c1bf2f35200e18
Fractalism <fractalism.cs05@nctu.edu.tw>
2023-07-09 08:29:03 +0800

    restyle weights section

--- src/app/minigame/weights.tsx
+++ src/app/minigame/weights.tsx
@@ -3,33 +3,33 @@ import styles from "./page.module.scss";
 export function Weights() {
   return (
     <div className={styles.weights}>
-      <WeightRow className={styles.diamond_x100_5000} />
-      <WeightRow className={styles.white_feather_x1} />
-      <WeightRow className={styles.purple_feather_x1} />
-      <WeightRow className={styles.fire_stone_x1000} />
-      <WeightRow className={styles.event_dice_x1} />
-      <WeightRow className={styles.earth_stone_x1000} />
-      <WeightRow className={styles.white_feather_x1_3} />
-      <WeightRow className={styles.wind_stone_x1000} />
-      <WeightRow className={styles.emerald_x150} />
-      <WeightRow className={styles.green_feather_x1} />
-      <WeightRow className={styles.water_stone_x1000} />
-      <WeightRow className={styles.emerald_x30_1500} />
-      <WeightRow className={styles.diamond_x500} />
+      <WeightItem className={styles.diamond_x100_5000} />
+      <WeightItem className={styles.white_feather_x1} />
+      <WeightItem className={styles.purple_feather_x1} />
+      <WeightItem className={styles.fire_stone_x1000} />
+      <WeightItem className={styles.event_dice_x1} />
+      <WeightItem className={styles.earth_stone_x1000} />
+      <WeightItem className={styles.white_feather_x1_3} />
+      <WeightItem className={styles.wind_stone_x1000} />
+      <WeightItem className={styles.emerald_x150} />
+      <WeightItem className={styles.green_feather_x1} />
+      <WeightItem className={styles.water_stone_x1000} />
+      <WeightItem className={styles.emerald_x30_1500} />
+      <WeightItem className={styles.diamond_x500} />
     </div>
   );
 }
 
-function WeightRow({ className }: { className: string }) {
+function WeightItem({ className }: { className: string }) {
   return (
-    <>
-      <div className={`${styles.block} ${className}`} />
+    <div className={styles.weightItem}>
+      <div className={`${styles.weightIcon} ${className}`} />
       <input
         className={styles.weightInput}
         type="text"
         value={123}
         placeholder="0"
       />
-    </>
+    </div>
   );
 }
