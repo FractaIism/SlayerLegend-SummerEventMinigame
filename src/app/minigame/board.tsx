380fdebd1fbe2188793cd7513ef6cabb008301fc
Fractalism <fractalism.cs05@nctu.edu.tw>
2023-07-11 00:46:25 +0800

    show next moves on board

--- src/app/minigame/board.tsx
+++ src/app/minigame/board.tsx
@@ -6,13 +6,16 @@ import { useContext, ReactNode, MouseEventHandler } from "react";
 import { range } from "lodash";
 import { GlobalVariableContext } from "./context.tsx";
 import { Position } from "./slayer.tsx";
+import { positionToIndex } from "./utils.tsx";
 
 const boardSize = 250;
 
 export function GameBoard({
+  slayerPosition,
   setSlayerPosition,
   children,
 }: {
+  slayerPosition: Position;
   setSlayerPosition: (position: Position) => void;
   children: ReactNode;
 }) {
@@ -24,7 +27,7 @@ export function GameBoard({
         width={boardSize}
         height={boardSize}
       />
-      <DecorativeSVGOverlay />
+      <DecorativeSVGOverlay slayerPosition={slayerPosition} />
       <FunctionalSVGOverlay setSlayerPosition={setSlayerPosition} />
       {children}
     </>
@@ -61,26 +64,51 @@ function FunctionalSVGOverlay({
   );
 }
 
-function DecorativeSVGOverlay() {
+function DecorativeSVGOverlay({
+  slayerPosition,
+}: {
+  slayerPosition: Position;
+}) {
   function blockExists(row: number, col: number) {
     return validPositions.some(({ row: r, col: c }) => r === row && c === col);
   }
 
+  function isBlockReachable(
+    blockIndex: number,
+    slayerIndex: number,
+    moves: number[],
+  ) {
+    const teleportBlocks: { [key: number]: number } = { 7: 2, 14: 9 };
+    const nextSlayerIndexes = moves.map((n) => (slayerIndex + n) % 16);
+    const nextSlayerIndexes2 = nextSlayerIndexes.map((idx) =>
+      idx in teleportBlocks ? teleportBlocks[idx] : idx,
+    );
+    return nextSlayerIndexes2.includes(blockIndex);
+  }
+
   const { validPositions } = useContext(GlobalVariableContext);
+
+  const slayerIndex = positionToIndex(slayerPosition);
 
   return (
     <svg className={styles.overlay} width={boardSize} height={boardSize}>
       {range(5).map((row) =>
-        range(5).map((col) =>
-          blockExists(row, col) ? (
-            <SVGRectBlock
-              key={`${row}-${col}`}
-              row={row}
-              col={col}
-              className={styles.overlayBlockVisible}
-            />
-          ) : null,
-        ),
+        range(5).map((col) => {
+          const blockIndex = positionToIndex({ row, col });
+          const r145 = isBlockReachable(blockIndex, slayerIndex, [1, 4, 5]);
+          const r236 = isBlockReachable(blockIndex, slayerIndex, [2, 3, 6]);
+          const blockClassName = r145 ? styles.cyan : r236 ? styles.violet : "";
+          if (blockExists(row, col)) {
+            return (
+              <SVGRectBlock
+                key={`${row}-${col}`}
+                row={row}
+                col={col}
+                className={`${styles.overlayBlockVisible} ${blockClassName}`}
+              />
+            );
+          }
+        }),
       )}
     </svg>
   );
--- src/app/minigame/page.tsx
+++ src/app/minigame/page.tsx
@@ -17,7 +17,10 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Providers>
-        <GameBoard setSlayerPosition={setSlayerPosition}>
+        <GameBoard
+          slayerPosition={slayerPosition}
+          setSlayerPosition={setSlayerPosition}
+        >
           <Slayer position={slayerPosition} />
         </GameBoard>
         <Weights />
